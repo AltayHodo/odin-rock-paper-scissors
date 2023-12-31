afc0e78c5ed32b8411257df6e9ae62edc6d97bb6
Altay Hodoglugil <altay.hodo@gmail.com>
2023-12-30 19:36:02 -0800

    Add game over and reset functionality

--- script.js
+++ script.js
@@ -3,27 +3,43 @@ const paperBtn = document.querySelector('.paper-btn');
 const scissorsBtn = document.querySelector('.scissors-btn');
 const buttons = document.querySelectorAll('button');
 const resultContainer = document.querySelector('.result');
+const winMessage = document.querySelector('.win-message');
 let playerSelection = '';
 let computerSelection = '';
 let playerWins = 0;
 let computerWins = 0;
 
-buttons.forEach(button => {
-  button.addEventListener('click', (e) => {
-    updatePlayerSelection(e);
-    updateComputerSelection();
-    playRound(playerSelection, computerSelection);
-  });
-});
+window.addEventListener('load', addButtonEvents);
 
-function updatePlayerSelection(e){
+
+function addButtonEvents(){
+  buttons.forEach(button => {
+    button.addEventListener('click', buttonClickHandler);
+  })
+}
+
+function buttonClickHandler(e){
+  updatePlayerSelection(e);
+  updateComputerSelection();
+  playRound(playerSelection, computerSelection);
+}
+
+function removeButtonEvents(){
+  buttons.forEach(button => {
+    button.removeEventListener('click', buttonClickHandler);
+  });
+}
+
+
+
+function updatePlayerSelection(e) {
   playerSelection = e.target.textContent.toLowerCase();
 }
 
 function updateComputerSelection() {
   const rand = Math.floor(Math.random() * 3) + 1;
   if (rand === 1) {
-    computerSelection =  'rock'
+    computerSelection = 'rock'
   } else if (rand === 2) {
     computerSelection = 'paper'
   } else {
@@ -37,21 +53,22 @@ function playRound(playerSelection, computerSelection) {
     playerSelection === 'scissors' && computerSelection === 'paper') {
     updateScore('playerWin');
     updateDisplay('playerWin');
-  } 
+  }
   else if (playerSelection === 'rock' && computerSelection === 'paper' ||
     playerSelection === 'paper' && computerSelection === 'scissors' ||
     playerSelection === 'scissors' && computerSelection === 'rock') {
     updateScore('computerWin');
     updateDisplay('computerWin');
   }
-  else{
+  else {
     updateScore('tie');
     updateDisplay('tie');
   }
+  checkGameOver();
 }
 
-function updateScore(outcome){
-  switch(outcome){
+function updateScore(outcome) {
+  switch (outcome) {
     case 'playerWin':
       playerWins++;
       console.log(`player wins: ${playerWins}`);
@@ -66,8 +83,8 @@ function updateScore(outcome){
   }
 }
 
-function updateDisplay(outcome){
-  switch(outcome){
+function updateDisplay(outcome) {
+  switch (outcome) {
     case 'playerWin':
       resultContainer.textContent = `
         You win! ${playerSelection} beats ${computerSelection}.
@@ -81,15 +98,39 @@ function updateDisplay(outcome){
         Computer score: ${computerWins}`;
       break;
     case 'tie':
-      resultContainer.textContent = 
-      `It's a tie! 
+      resultContainer.textContent =
+        `It's a tie! 
       Player score: ${playerWins}
       Computer score: ${computerWins}`;
       break;
   }
 }
 
+function checkGameOver() {
+  if (playerWins === 5 || computerWins === 5) {
+    gameOver();
+  }
+}
 
+function gameOver() {
+  if (playerWins === 5) {
+    winMessage.textContent = 'You won the game!';
+  } else {
+    winMessage.textContent = 'You lost the game!';
+  }
+  const resetButton = document.createElement('button');
+  resetButton.textContent = 'Reset';
+  winMessage.appendChild(resetButton);
+  resetButton.addEventListener('click', resetGame);
+  removeButtonEvents();
+}
+
+function resetGame() {
+  playerWins = 0;
+  computerWins = 0;
+  winMessage.textContent = '';
+  resultContainer.textContent = '';
+}
 
 
 
